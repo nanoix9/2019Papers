f7d80d6bc8714a687017156052024630f5f9cdae
nanoix9 <nanoix9@gmail.com>
2020-06-06 23:35:43 +1200

    dw query question 3

--- dw/ass2/queriesDW.sql
+++ dw/ass2/queriesDW.sql
@@ -43,10 +43,74 @@ sr AS (
 SELECT store_name, total_sales, ROWNUM AS rank
 FROM sr
 WHERE ROWNUM = 1;
+/
 
 -- Q3 Determine the top 3 products for a month (say, Dec 2019), and 
 -- for the 2 months before that, in terms of total sales.
 -- PRODUCT_NAME SUM(TOTAL_SALES) RANK 
+
+DROP TYPE T_TABLE_COLL;
+DROP TYPE T_TABLE;
+/
+CREATE OR REPLACE TYPE T_TABLE IS OBJECT
+(
+    product_name VARCHAR2(30),
+    total_sales NUMBER,
+    rank NUMBER,
+    month NUMBER,
+    year NUMBER
+);
+/
+CREATE OR REPLACE TYPE T_TABLE_COLL IS TABLE OF T_TABLE;
+/
+CREATE OR REPLACE
+FUNCTION top_in_months(
+    top_k IN NUMBER,
+    start_month IN NUMBER, 
+    start_year IN NUMBER,
+    num_months IN NUMBER)
+RETURN T_TABLE_COLL PIPELINED
+IS
+    CURSOR top_products (month_in IN NUMBER, year_in IN NUMBER)
+    IS 
+        SELECT product_name, total_sales, ROWNUM AS rank, month, year
+        FROM (
+            SELECT product_name, total_sales, ROWNUM AS rank, month, year
+            FROM (SELECT product_name, SUM(amount) AS total_sales, month, year
+                FROM Sales s, DateDim d, Product p
+                WHERE s.date_id = d.date_id 
+                    AND s.product_id = p.product_id
+                    AND month = month_in
+                    AND year = year_in
+                GROUP BY product_name, month, year)
+            ORDER BY total_sales DESC
+        )
+        WHERE ROWNUM <= top_k;
+    
+    query_month NUMBER := 0;
+    query_year NUMBER := 0;
+BEGIN
+    query_month := start_month;
+    query_year := start_year;
+
+    FOR k in 1..top_k
+    LOOP
+        FOR i IN top_products(query_month, query_year)
+        LOOP
+            PIPE ROW (T_TABLE(i.product_name, i.total_sales, i.rank, i.month, i.year));
+        END LOOP;
+
+        IF query_month = 1 THEN
+            query_month := 12;
+            query_year := query_year - 1;
+        ELSE
+            query_month := query_month - 1;
+        END IF;
+    END LOOP;
+END;
+/
+SELECT * FROM TABLE(top_in_months(3, 12, 2019, 3));
+/
 
 -- Q4 Create a materialised view called “STOREANALYSIS” that presents 
 -- the product-wise sales analysis for each store. The results should be ordered 
@@ -60,6 +124,21 @@ SELECT store_id, product_id, SUM(amount)
 FROM Sales
 ORDER BY store_id, product_id;
 
+CREATE OR REPLACE
+FUNCTION foo()
+RETURN T_TABLE_COLL
+IS
+    top_products T_TABLE_COLL;
+begin
+    SELECT T_TABLE (product_id, amount)
+    INTO top_products
+    FROM Sales 
+    WHERE date_id = '20190505';
+    return(top_products);
+end;
+
 -- Q5 Think about what information can be retrieved from the materialised view
 -- created in Q4 using ROLLUP or CUBE concepts and provide some useful information
 -- of your choice for management.
+
+
